d5c6a70c79f77cdc90cf0e4df95588ac5ebc6ffe
Brit Sovic <bsovic@tulane.edu>
2022-12-01 19:05:49 -0500

    updated api routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -4,10 +4,13 @@ const { createNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
 // note routes
+
+// Get notes
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
+// Post note
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
 
