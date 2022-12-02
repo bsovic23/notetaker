b4d3670dbbee9edf6b6a4db3db63e7e2c392ab66
Brit Sovic <bsovic@tulane.edu>
2022-12-01 19:12:24 -0500

    updating server

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
+// packages
 const notes = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
 
+// routes
 const noteRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+// setting up server
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Middleware
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', noteRoutes);
 app.use('/', htmlRoutes);
 
-//
+// 
 app.listen(3001, (req, res) =>{
     console.log(`API now on port ${PORT}!`);
 });
